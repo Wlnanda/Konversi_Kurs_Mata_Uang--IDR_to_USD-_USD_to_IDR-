d2eb4ecc7ea1cda3e33aec612e7acd5519528de0
Wind <133625558+Winanda-QS@users.noreply.github.com>
2023-06-02 10:58:10 +0700

    Create individu_2113010286.java
    Individu Winanda

---
+++ individu_2113010286.java
@@ -0,0 +1,9 @@
+public class individu_2113010286 {
+    public static void main(String[] args) {
+        String nama = "Winanda Qoulan Syadida";
+        String nim = "2113010286";
+        
+        System.out.println("Name: " + nama);
+        System.out.println("NIM: " + nim);
+    }
+}
